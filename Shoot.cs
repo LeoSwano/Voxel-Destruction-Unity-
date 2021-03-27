9738acd5d9f1a3054c8f34d7a7731683e916e7de
LeoSwano <72434467+LeoSwano@users.noreply.github.com>
2021-03-28 00:51:33 +0100

    Add files via upload

---
+++ Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+	public GameObject projectile;
+	public int speed;
+	public GameObject bulletExit;
+
+
+	void Update()
+	{		
+		if (Input.GetMouseButtonDown(0))
+		{
+			//anim.SetTrigger("hit");
+			GameObject bul = (GameObject)Instantiate(projectile, bulletExit.transform.position, Quaternion.identity);
+			bul.gameObject.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * speed;
+		}
+	}
+}
