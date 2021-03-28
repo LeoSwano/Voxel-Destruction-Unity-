a0897a69d4bbb73221cce54edb509a12f4ef8059
LeoSwano <72434467+LeoSwano@users.noreply.github.com>
2021-03-28 13:38:33 +0200

    Add files via upload

---
+++ Collision.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+	private Collider[] hitColliders;
+
+	public float blastRadius;
+	public float explosionPower;
+	public LayerMask explosionLayers;
+
+	public GameObject Particles;
+	
+	private void OnCollisionEnter(UnityEngine.Collision col)
+	{
+		Particles.gameObject.GetComponent<Renderer>().material = col.gameObject.GetComponent<MeshRenderer>().material;
+		Instantiate(Particles, col.transform.position, Quaternion.identity);
+
+		destroy(col.contacts[0].point);			
+	}
+
+	void destroy(Vector3 explosionPoint)
+	{
+		hitColliders = Physics.OverlapSphere(explosionPoint, blastRadius, explosionLayers);
+
+		foreach (Collider hitCol in hitColliders)
+		{
+			if (hitCol.GetComponent<Rigidbody>() == null)
+			{
+				hitCol.GetComponent<MeshRenderer>().enabled = true;
+				hitCol.gameObject.AddComponent<Rigidbody>();
+
+
+				hitCol.GetComponent<Rigidbody>().mass = 500;
+				hitCol.GetComponent<Rigidbody>().isKinematic = false;
+
+				hitCol.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * 5;
+				hitCol.GetComponent<Rigidbody>().AddExplosionForce(explosionPower, explosionPoint, blastRadius, 1, ForceMode.Impulse);
+			}
+		}
+	}
+}
---
+++ Destruction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Destruction : MonoBehaviour
+{
+	public GameObject mesh;
+
+	float cubeWidth;
+	float cubeHeight;
+	float cubeDepth;
+
+	public float cubeScale = 0.3f;
+
+	void Start()
+	{
+		cubeWidth = transform.localScale.z;
+		cubeHeight = transform.localScale.y;
+		cubeDepth = transform.localScale.x;
+
+		
+
+		gameObject.GetComponent<MeshRenderer>().enabled = false;
+		mesh.gameObject.GetComponent<Transform>().localScale = new Vector3(cubeScale, cubeScale, cubeScale);
+		
+	}
+
+	private void OnCollisionEnter(UnityEngine.Collision collision)
+	{
+		if (collision.gameObject.tag == "Projectile"){ 
+			CreateCube();
+		}
+	}
+
+	void CreateCube()
+	{
+		this.gameObject.GetComponent<BoxCollider>().enabled = false;
+
+		this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+
+
+		if (gameObject.CompareTag("box"))
+		{
+			for (float x = 0; x < cubeWidth; x += cubeScale)
+			{
+				for (float y = 0; y < cubeHeight; y += cubeScale)
+				{
+					for (float z = 0; z < cubeDepth; z += cubeScale)
+					{
+						Vector3 vec = transform.position;
+
+						GameObject cubes = (GameObject)Instantiate(mesh, vec + new Vector3(x, y, z), Quaternion.identity);
+						cubes.gameObject.GetComponent<MeshRenderer>().material = gameObject.GetComponent<MeshRenderer>().material;
+					}
+				}
+			}
+		}
+	}
+}
